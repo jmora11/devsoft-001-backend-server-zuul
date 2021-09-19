391320c21bea8f7a10079cf976884ddf54024df6
Julian Mora <juli.mora.5667@gmail.com>
2021-09-19 13:27:03 -0500

    complete dev

---
+++ src/test/java/com/P001SrpingBoot/back/zuul/ZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P001SrpingBoot.back.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
